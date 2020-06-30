36b055603653aaadb792d381163231629fa95a56
takuya617 <nishihara0617@gmail.comcdgit config --global user.email nishihara0617@gmail.com>
2020-06-30 19:34:55 +0900

    generated files by scaffold

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -1,8 +1,8 @@
-$(function(){ 
+$(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="Right-side__message-room">
+        `<div class="Right-side__message-room" data-message-id=${message.id}>
           <div class="Right-side__messageInfo">
             <div class="messageInfo__name">
               ${message.user_name}
@@ -12,7 +12,7 @@ $(function(){
             </div>
           </div>
           <div class="Right-side__text">
-            <p class="text__content ">
+            <p class="text__content">
               ${message.content}
             </p>
             <img class="Message__image" src="${message.image}">
@@ -21,17 +21,17 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="Right-side__message-room">
+      `<div class="Right-side__message-room" data-message-id=${message.id}>
         <div class="Right-side__messageInfo">
           <div class="messageInfo__name">
             ${message.user_name}
           </div>
-          <div class="messageInfo__day ">
+          <div class="messageInfo__day">
             ${message.created_at}
           </div>
         </div>
         <div class="Right-side__text">
-          <p class="text__content ">
+          <p class="text__content">
             ${message.content}
           </p>
         </div>
@@ -40,27 +40,27 @@ $(function(){
     };
   }
 
-  $(".Form").on("submit", function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+  let reloadMessages = function() {
+    let last_message_id = $('.Right-side__message-room:last').data("message-id");
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      url: "api/messages",
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+      data: {id: last_message_id}
     })
-    .done(function(data){
-      let html = buildHTML(data);
-      $(".Right-side__main-list").append(html);
-      $("form")[0].reset();
-      $('.Right-side__main-list').animate({ scrollTop: $('.Right-side__main-list')[0].scrollHeight});
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        let insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.Right-side__main-list').append(insertHTML);
+        $('.Right-side__main-list').animate({ scrollTop: $('.Right-side__main-list')[0].scrollHeight});
+      }
     })
     .fail(function() {
-        alert("メッセージ送信に失敗しました");
+      alert('error');
     });
-    return false;
-  });
+  };
+  setInterval(reloadMessages, 7000);
 });
